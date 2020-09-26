2184b4f38ef938f5355a3cbb168736d1d43a0bfc
daichi_sato <daichiexperience@gmail.com>
2020-09-26 13:43:38 +0900

    [fix] App.js, App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Hello world!</h1>
     </div>
   );
-}
+};
 
 export default App;
